366fd1b900aab6207f687b6ab44523dcde5227dd
sandy <nasdrahlovejinx@hotmail.com>
2025-03-12 10:19:34 +0100

    creating springboot structure and adding pom libreries

---
+++ src/test/java/api_recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package api_recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
