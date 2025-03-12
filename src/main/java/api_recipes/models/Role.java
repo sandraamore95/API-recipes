c035cac6ceef6d1f5e4e2bf46824d6b1df0907c0
sandy <nasdrahlovejinx@hotmail.com>
2025-03-12 16:22:15 +0100

    addding User and Role models

---
+++ src/main/java/api_recipes/models/Role.java
@@ -0,0 +1,28 @@
+package api_recipes.models;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter @NoArgsConstructor @AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private RoleName name;
+
+    public enum RoleName {
+        ROLE_USER,
+        ROLE_ADMIN
+    }
+
+    // Constructor con solo el nombre del rol
+    public Role(RoleName name) {
+        this.name = name;
+    }
+}
