1c4f6fa8f632bccf8d919a19b256d92bd7e30947
sandy <nasdrahlovejinx@hotmail.com>
2025-03-13 20:09:34 +0100

    implement TestController

--- src/main/java/api_recipes/controllers/AuthController.java
+++ src/main/java/api_recipes/controllers/AuthController.java
@@ -57,7 +57,7 @@ public class AuthController {
     //LOGIN
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-
+logger.error(loginRequest.getUsername());
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
