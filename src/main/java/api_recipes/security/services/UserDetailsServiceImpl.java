97545c1eb610eafe8e536d36062123d5b25e8092
sandy <nasdrahlovejinx@hotmail.com>
2025-03-13 11:16:52 +0100

    implement UserRepository and UserDetailsServiceImpl

---
+++ src/main/java/api_recipes/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package api_recipes.security.services;
+
+import api_recipes.models.User;
+import api_recipes.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+
+        //RETURNA EL USERNAME del usuario para que despues en AuthTokenFilter  a traves de su username pueda sacar el TOKEN.
+    }
+}
