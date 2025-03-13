456ebc23711c6ed82bfde235401c2816ba874ebf
sandy <nasdrahlovejinx@hotmail.com>
2025-03-13 13:53:04 +0100

    implement RoleRepository

---
+++ src/main/java/api_recipes/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package api_recipes.repository;
+import api_recipes.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(Role.RoleName name);
+}
