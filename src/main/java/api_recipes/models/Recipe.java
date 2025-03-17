055cb5dfe2ccc0f4037d6ebc7c55021462d46353
sandy <nasdrahlovejinx@hotmail.com>
2025-03-18 00:27:59 +0100

    Âimplement Recipe model

---
+++ src/main/java/api_recipes/models/Recipe.java
@@ -0,0 +1,38 @@
+package api_recipes.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Table(name = "recipes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Ingredient> ingredients;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;  // Usuario que creó la receta
+
+    public enum Category {
+        SALUDABLE, SENCILLA, CENAR, COMER, POSTRE, VEGETARIANA,DESAYUNO
+    }
+
+}
