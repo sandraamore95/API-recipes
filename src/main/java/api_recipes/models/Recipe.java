ecb9f70b6f8c397f834d09c6c04df4056c75b317
sandy <nasdrahlovejinx@hotmail.com>
2025-03-21 16:53:48 +0100

    modified categories enum  to collection

--- src/main/java/api_recipes/models/Recipe.java
+++ src/main/java/api_recipes/models/Recipe.java
@@ -1,9 +1,11 @@
 package api_recipes.models;
-
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "recipes")
@@ -23,18 +25,35 @@ public class Recipe {
     @Column(length = 1000)
     private String description;
 
+    @ElementCollection(targetClass = Category.class)
+    @CollectionTable(name = "recipe_categories", joinColumns = @JoinColumn(name = "recipe_id"))
+    @Column(name = "category", nullable = false)
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Category category;
+    private Set<Category> categories = new HashSet<>();
 
     //lista de ingredientes (entidad)
     @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Ingredient> ingredients;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;  // Usuario que creó la receta
 
+    public void addCategory(Category category) {
+        categories.add(category);
+    }
+
+    public void removeCategory(Category category) {
+        categories.remove(category);
+    }
+
+    public boolean hasCategory(Category category) {
+        return categories.contains(category);
+    }
+
+    public Set<Category> getCategories() {
+        return Collections.unmodifiableSet(categories);
+    }
     public enum Category {
         SALUDABLE, SENCILLA, CENAR, COMER, POSTRE, VEGETARIANA,DESAYUNO
     }
