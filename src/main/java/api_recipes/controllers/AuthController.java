c2723a807b7d5feb0b7f02f3e51224c9773865fc
sandy <nasdrahlovejinx@hotmail.com>
2025-04-25 17:57:05 +0200

    refactoring authcontroller

--- src/main/java/api_recipes/controllers/AuthController.java
+++ src/main/java/api_recipes/controllers/AuthController.java
@@ -3,7 +3,6 @@ import api_recipes.exceptions.InvalidRequestException;
 import api_recipes.exceptions.InvalidTokenException;
 import api_recipes.exceptions.ResourceAlreadyExistsException;
 import api_recipes.exceptions.ResourceNotFoundException;
-import api_recipes.models.Role;
 import api_recipes.models.User;
 import api_recipes.payload.request.ForgotPasswordRequest;
 import api_recipes.payload.request.LoginRequest;
@@ -12,11 +11,10 @@ import api_recipes.payload.request.SignupRequest;
 import api_recipes.payload.response.ErrorResponse;
 import api_recipes.payload.response.JwtResponse;
 import api_recipes.payload.response.SuccessResponse;
-import api_recipes.repository.RoleRepository;
-import api_recipes.repository.UserRepository;
 import api_recipes.security.jwt.JwtUtils;
 import api_recipes.security.services.UserDetailsImpl;
 import api_recipes.services.AccountService;
+import api_recipes.services.UserService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,10 +29,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
@@ -44,21 +39,18 @@ public class AuthController {
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     private final AuthenticationManager authenticationManager;
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
+    private final UserService userService;
     private final PasswordEncoder encoder;
     private final JwtUtils jwtUtils;
     private final AccountService accountService;
 
     public AuthController(AuthenticationManager authenticationManager,
-                          UserRepository userRepository,
-                          RoleRepository roleRepository,
+                          UserService userService,
                           PasswordEncoder encoder,
                           JwtUtils jwtUtils,
                           AccountService accountService) {
         this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
+        this.userService=userService;
         this.encoder = encoder;
         this.jwtUtils = jwtUtils;
         this.accountService=accountService;
@@ -89,45 +81,21 @@ public class AuthController {
     //REGISTER
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-
-        // Verificar si el nombre de usuario ya está registrado
-        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-            throw new ResourceAlreadyExistsException("Error: El nombre de usuario ya está en uso");
+        try {
+            User newUser = userService.registerUser(signUpRequest);
+            return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+        } catch (ResourceAlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorResponse("Recurso no encontrado", e.getMessage()));
+        } catch (InvalidRequestException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ErrorResponse("Error de validación", e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorResponse("Error al crear usuario", "Ocurrió un error inesperado"));
         }
-        // Verificar si el email ya está registrado
-        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-            throw new ResourceAlreadyExistsException("Error: ¡El email  ya está en uso!");
-        }
-
-        // Crear cuenta de nuevo usuario
-        User user = new User(signUpRequest.getUsername(),
-                signUpRequest.getEmail(),
-                encoder.encode(signUpRequest.getPassword()));
-
-        // Asignar roles
-        Set<String> strRoles = signUpRequest.getRole();
-        Set<Role> roles = new HashSet<>();
-        if (strRoles == null || strRoles.isEmpty()) {
-            // Asignar rol por defecto ROLE_USER
-            Role userRole = roleRepository.findByName(Role.RoleName.ROLE_USER)
-                    .orElseThrow(() -> new InvalidRequestException("El rol ROLE_USER no fue encontrado."));
-            roles.add(userRole);
-        } else {
-            for (String role : strRoles) {
-                try {
-                    Role.RoleName roleName = Role.RoleName.valueOf(role.toUpperCase());
-                    Role foundRole = roleRepository.findByName(roleName)
-                            .orElseThrow(() -> new InvalidRequestException("El rol " + role + " no fue encontrado."));
-                    roles.add(foundRole);
-                } catch (IllegalArgumentException e) {
-                    throw new InvalidRequestException("Rol inválido: " + role);
-                }
-            }
-        }
-        user.setRoles(roles);
-        userRepository.save(user);
-        return ResponseEntity.ok(new SuccessResponse("¡Usuario registrado exitosamente!"));
     }
+
 
     //forgot password
     @PostMapping("/forgot-password")
--- src/main/java/api_recipes/services/UserService.java
+++ src/main/java/api_recipes/services/UserService.java
@@ -1,18 +1,21 @@
 package api_recipes.services;
+import api_recipes.exceptions.InvalidRequestException;
 import api_recipes.exceptions.InvalidTokenException;
+import api_recipes.exceptions.ResourceAlreadyExistsException;
 import api_recipes.mapper.UserMapper;
+import api_recipes.models.Role;
 import api_recipes.models.TokenUser;
 import api_recipes.models.User;
 import api_recipes.payload.dto.UserDto;
+import api_recipes.payload.request.SignupRequest;
+import api_recipes.repository.RoleRepository;
 import api_recipes.repository.TokenUserRepository;
 import api_recipes.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,13 +23,15 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-    private final TokenUserRepository tokenUserRepository;
+    private final  RoleRepository roleRepository;
+    private final  PasswordEncoder encoder;
 
 
-    public UserService(UserRepository userRepository,UserMapper userMapper,TokenUserRepository tokenUserRepository) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, RoleRepository roleRepository,PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userMapper=userMapper;
-        this.tokenUserRepository=tokenUserRepository;
+        this.roleRepository=roleRepository;
+        this.encoder=passwordEncoder;
     }
 
     // Obtener todos los usuarios
@@ -41,6 +46,50 @@ public class UserService {
     public Optional<UserDto> getUserById(Long id) {
         return userRepository.findById(id).map(userMapper::toDTO);
     }
+
+    //Crear usuario
+    public User registerUser(SignupRequest signUpRequest) {
+        // Verificar si el nombre de usuario ya está registrado
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            throw new ResourceAlreadyExistsException("Error: El nombre de usuario ya está en uso");
+        }
+
+        // Verificar si el email ya está registrado
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            throw new ResourceAlreadyExistsException("Error: ¡El email ya está en uso!");
+        }
+
+        // Crear cuenta de nuevo usuario
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        // Asignar roles
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+        if (strRoles == null || strRoles.isEmpty()) {
+            // Asignar rol por defecto ROLE_USER
+            Role userRole = roleRepository.findByName(Role.RoleName.ROLE_USER)
+                    .orElseThrow(() -> new InvalidRequestException("El rol ROLE_USER no fue encontrado."));
+            roles.add(userRole);
+        } else {
+            for (String role : strRoles) {
+                try {
+                    Role.RoleName roleName = Role.RoleName.valueOf(role.toUpperCase());
+                    Role foundRole = roleRepository.findByName(roleName)
+                            .orElseThrow(() -> new InvalidRequestException("El rol " + role + " no fue encontrado."));
+                    roles.add(foundRole);
+                } catch (IllegalArgumentException e) {
+                    throw new InvalidRequestException("Rol inválido: " + role);
+                }
+            }
+        }
+
+        user.setRoles(roles);
+        return userRepository.save(user);
+    }
+
+
 
     // Actualizar usuario
     public Optional<UserDto> updateUser(Long id, UserDto userDto) {
