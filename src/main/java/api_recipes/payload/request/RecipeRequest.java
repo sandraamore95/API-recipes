5dd1b2301b02b28f1d1ec63fd9e861bcacca0694
sandy <nasdrahlovejinx@hotmail.com>
2025-06-06 18:03:26 +0200

    Modified max length for descripcion and preparation at DTO

--- src/main/java/api_recipes/payload/request/RecipeRequest.java
+++ src/main/java/api_recipes/payload/request/RecipeRequest.java
@@ -14,11 +14,11 @@ public class RecipeRequest {
     private String title;
 
     @NotBlank(message = "La descripción es obligatoria")
-    @Size(min = 10, max = 100, message = "La descripción debe tener entre 10 y 100 caracteres")
+    @Size(min = 10, max = 1000, message = "La descripción debe tener entre 10 y 100 caracteres")
     private String description;
 
     @NotBlank(message = "El método de preparación es obligatorio")
-    @Size(min = 5, max = 100, message = "El método de preparación debe tener entre 5 y 100 caracteres")
+    @Size(min = 5, max = 1000, message = "El método de preparación debe tener entre 5 y 100 caracteres")
     private String preparation;
 
     private Set<String> categories; // la receta se puede crear sin categorias
